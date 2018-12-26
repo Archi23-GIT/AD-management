2a7ddcfd0418416d0c2bacd84843431779c0d10f
Archi23-GIT <46132222+Archi23-GIT@users.noreply.github.com>
2018-12-26 12:32:56 +0100

    Delete zimbra-conn.php

--- zimbra-conn.php
+++
@@ -1,6 +0,0 @@
-<?php
-$zimbraAdminEmail = getCNofDN($_SESSION['username'])."@".$_SESSION['mailDomain'];
-$zimbraAdminPassword = $_SESSION['password'];
-$api = \Zimbra\Admin\AdminFactory::instance('https://'.$_SESSION['mailserver'].':7071/service/admin/soap');
-$api->auth($zimbraAdminEmail, $zimbraAdminPassword);
-?>
